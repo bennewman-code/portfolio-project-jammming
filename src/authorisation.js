a7858be4e02f8604cf2e6136ac4084c0bf256f0d
bennewman-code <51948601+bennewman-code@users.noreply.github.com>
2025-02-24 21:33:57 +1000

    Update authorisation.js
    authorize with actual clientId and redirectUrl

--- src/authorisation.js
+++ src/authorisation.js
@@ -7,8 +7,8 @@
  * https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
  */
 
-const clientId = 'yourClientIDGoesHere'; // your clientId
-const redirectUrl = 'eg:http://localhost:8080';        // your redirect URL - must be localhost URL and/or HTTPS
+const clientId = '03ce66bd50a54bb78f5d89c1c401407e'; // your clientId
+const redirectUrl = 'http://localhost:3000';        // your redirect URL - must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
