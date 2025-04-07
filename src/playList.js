3730e4a42fb023396ee7702b4a3f12b33e30e986
bennewman-code <51948601+bennewman-code@users.noreply.github.com>
2025-04-07 22:13:53 +1000

    Playlist added
    Not fully working yet but lets us access a state in a function in another file

--- src/playList.js
+++ src/playList.js
@@ -1,4 +1,22 @@
+import React, { useContext } from 'react';
+import { SongContext } from './SongContext';
+
+const PlayList = () =>  {
+    const { addedSongs } = useContext(SongContext);
+    // the h2 Playlist shows up in both login and logout states (it should only show up when logged in need to fix)
+    //the ul bit doesnt work so ill have to try some stuff
+    /*return (
+        <div>
+            <h2>Playlist</h2>
+            <ul>
+                {addedSongs.map((song, index) => (
+                    <li key={ index }>{ song[0] }</li>
+                ))}
+            </ul>
+        </div>
+    )*/
+}
 
 
 
-
+export default PlayList;
