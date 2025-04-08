518a3814d91229407e7f70821598fc1d467c0fea
bennewman-code <51948601+bennewman-code@users.noreply.github.com>
2025-04-08 20:21:17 +1000

    Update playList.js
    Added the ability to remove song from the state

--- src/playList.js
+++ src/playList.js
@@ -2,20 +2,29 @@ import React, { useContext } from 'react';
 import { SongContext } from './SongContext';
 
 const PlayList = () =>  {
-    const { addedSongs } = useContext(SongContext);
+    const { addedSongs, setAddedSongs } = useContext(SongContext);
     console.log(addedSongs);
+
+    const handleClick = (indexToRemove) => {
+        setAddedSongs((prevAddedSongs) => prevAddedSongs.filter((_, index) => index !== indexToRemove));
+    }
     // the h2 Playlist shows up in both login and logout states (it should only show up when logged in need to fix)
     //the ul bit doesnt work so ill have to try some stuff
-    /*return (
+    return (
         <div>
             <h2>Playlist</h2>
-            <ul>
+            <div>
                 {addedSongs.map((song, index) => (
-                    <li key={ index }>{ song[0] }</li>
+                    <div key={ index }>
+                        <p>Name: { song[0] }</p>
+                        <p>Artist: { song[1].join(', ') }</p>
+                        <img src={ song[2].url } width={ song[2].width } height={ song[2].height } alt={ "Cover Art" }/>
+                        <button type="button" onClick={ () => handleClick(index) }>-</button>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
-    )*/
+    )
 }
 
 
